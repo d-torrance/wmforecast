b3d7ec564022925e62b6e491aa8db45ebdfbd39e
Doug Torrance <dtorrance@monmouthcollege.edu>
2014-10-29 12:42:15 -0500

    wmforecast.c: Use PACKAGE_STRING for --version; update to reflect GNU coding standards (http://www.gnu.org/prep/standards/html_node/_002d_002dversion.html).

--- wmforecast.c
+++ wmforecast.c
@@ -609,13 +609,12 @@ Preferences *setPreferences(int argc, char **argv)
 			break;
 
 		case 'v':
-			printf("wmforecast %s\n\n"
-			       "Copyright (C) 2014 Doug Torrance\n"
+			printf("%s\n"
+			       "Copyright © 2014 Doug Torrance\n"
 			       "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n"
 			       "This is free software: you are free to change and redistribute it.\n"
-			       "There is NO WARRANTY, to the extent permitted by law.\n\n"
-			       "Written by Doug Torrance\n"
-			       , VERSION);
+			       "There is NO WARRANTY, to the extent permitted by law.\n"
+			       , PACKAGE_STRING);
 			exit(0);
 
 		case 'h':
